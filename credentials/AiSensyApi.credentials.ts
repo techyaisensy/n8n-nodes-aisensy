454d56cc1353f75d816c3ab29fd4b85b83b1c0b0
Romit Singh Bhau <romit@aisensy.com>
2026-01-08 17:37:04 +0530

    Update: Added test cred request

--- credentials/AiSensyApi.credentials.ts
+++ credentials/AiSensyApi.credentials.ts
@@ -1,25 +1,39 @@
 import {
-	ICredentialType,
-	INodeProperties,
-} from 'n8n-workflow';
+  ICredentialType,
+  INodeProperties,
+  ICredentialTestRequest,
+} from "n8n-workflow";
 
 export class AiSensyApi implements ICredentialType {
-	name = 'aiSensyApi';
-	displayName = 'AiSensy API';
-	documentationUrl = 'https://wiki.aisensy.com/en/articles/11501891-how-to-setup-whatsapp-api-campaigns-in-aisensy';
-	properties: INodeProperties[] = [
-		{
-			displayName: 'API Key',
-			name: 'apiKey',
-			type: 'string',
-			typeOptions: {
-				password: true,
-			},
-			default: '',
-			description: 'Your AiSensy API key. Get it from AiSensy -> Open your project -> Go to manage page -> Copy API key',
-			required: true,
-		},
-	];
+  name = "aiSensyApi";
+  displayName = "AiSensy API";
+  documentationUrl =
+    "https://wiki.aisensy.com/en/articles/11501891-how-to-setup-whatsapp-api-campaigns-in-aisensy";
+  properties: INodeProperties[] = [
+    {
+      displayName: "API Key",
+      name: "apiKey",
+      type: "string",
+      typeOptions: {
+        password: true,
+      },
+      default: "",
+      description:
+        "Your AiSensy API key. Get it from AiSensy -> Open your project -> Go to manage page -> Copy API key",
+      required: true,
+    },
+  ];
+
+  test: ICredentialTestRequest = {
+    request: {
+      baseURL: "https://backend.aisensy.com",
+      url: "/campaign/t1/api/campaign-details",
+      method: "POST",
+      json: true,
+      body: {
+        apiKey: "={{$credentials.apiKey}}",
+        campaignName: "test",
+      },
+    },
+  };
 }
-
-
